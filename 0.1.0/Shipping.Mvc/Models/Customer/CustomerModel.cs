e9132ecdc23902525f6117e0205e676be2c3d3ab
aegean4112 <aegean4112@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2011-11-25 04:41:44 +0000

    customer controller, dkk

---
+++ 0.1.0/Shipping.Mvc/Models/Customer/CustomerModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shipping.Mvc.Models.Customer
+{
+    public class CustomerModel
+    {
+        [Required(ErrorMessage="Customer Name must be fill")]
+        [MinLength(5,ErrorMessage="Customer Name min 5 character")]
+        [DisplayName("Customer Name")]
+        public string CustomerName { get; set; }
+
+        public string Office { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+
+        [DisplayName("Contact Person")]
+        public string ContactPerson { get; set; }
+    }
+}
