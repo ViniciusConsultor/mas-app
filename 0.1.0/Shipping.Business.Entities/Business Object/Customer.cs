7ed199243dbe2172be6188348859e9d4aa8e99f8
aegean4112 <aegean4112@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2011-11-29 01:53:55 +0000

    add entitiy for customer

--- 0.1.0/Shipping.Business.Entities/Business Object/Customer.cs
+++ 0.1.0/Shipping.Business.Entities/Business Object/Customer.cs
@@ -2,18 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 
 namespace Shipping.Business.Entities
 {
+    [Table(Name="Customer")]
     public class Customer
     {
-        public virtual Guid Id { get; set; }
+        [Column(Name="customer_id", IsPrimaryKey=true)]
+        public Guid ID { get; set; }
+
+        [Column(Name="customer_name")]
         public string CustomerName { get; set; }
+
+        [Column(Name = "office")]
         public string Office { get; set; }
+
+        [Column(Name = "address")]
         public string Address { get; set; }
+
+        [Column(Name = "phone")]
         public string Phone { get; set; }
+
+        [Column(Name = "fax")]
         public string Fax { get; set; }
+
+        [Column(Name = "email")]
         public string Email { get; set; }
+
+        [Column(Name = "contact_person")]
         public string ContactPerson { get; set; }
     }
 }
