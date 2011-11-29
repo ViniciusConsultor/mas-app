df13c034429a03e1732500d8d361ef6cbbd04891
aegean4112 <aegean4112@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2011-11-29 01:54:18 +0000

    add service icustomer, dkk

---
+++ trunk/0.1.0/Shipping.Business.Services/CustomerService/CustomerService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shipping.Data;
+using Shipping.Business.Entities;
+
+namespace Shipping.Business.Services.CustomerService
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository) {
+            _customerRepository = customerRepository;
+        }
+
+        public bool CreateCustomer(Customer customer)
+        {
+            return _customerRepository.CreateCustomer(customer);
+        }
+
+
+        public IEnumerable<Customer> GetListCustomer()
+        {
+            return _customerRepository.GetListCustomer();
+        }
+    }
+}
