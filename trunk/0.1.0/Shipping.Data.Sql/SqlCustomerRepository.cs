e10fd16c99af17f2a7164feddc4ba5923099ef6b
aegean4112 <aegean4112@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2011-11-29 02:00:54 +0000

    add sqlcustomerrepository

---
+++ trunk/0.1.0/Shipping.Data.Sql/SqlCustomerRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shipping.Business.Entities;
+using System.Data.SqlClient;
+using Shipping.Web.Utility;
+using System.Data.Linq;
+
+namespace Shipping.Data.Sql
+{
+    public class SqlCustomerRepository : ICustomerRepository
+    {
+        private readonly String _mainConnectionString;
+
+        public SqlCustomerRepository(string mainConnectionString)
+        {
+            _mainConnectionString = mainConnectionString;
+        }
+
+        public bool CreateCustomer(Customer customer)
+        {
+            String sql = "Insert into Customer values(" + Utility.GetStringParameter(customer) + ")";
+            SqlParameter[] sqlParam = SqlUtility.SetSqlParamter(customer);
+            bool isSuccess = SqlUtility.CreateNewRecord(_mainConnectionString, sql, sqlParam);
+
+            if (isSuccess)
+                return true;
+
+            return false;
+        }
+
+
+        public IEnumerable<Customer> GetListCustomer()
+        {
+            DataContext dc = new DataContext(_mainConnectionString);
+            IEnumerable<Customer> listCustomer = null;
+
+            try {
+                listCustomer = dc.GetTable<Customer>().ToList() ;
+            }
+            catch(Exception e){
+                Console.WriteLine("SqlCustomerRepository.cs - GetListCustomer() " + e.InnerException);
+            }
+
+            return listCustomer;
+        }
+
+    }
+}
