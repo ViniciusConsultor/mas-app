a95b2c88be6ab4869900adc62d9ee6fba893ed8f
aegean4112 <aegean4112@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2011-11-29 02:00:10 +0000

    add utility

---
+++ 0.1.0/Shipping.Web/Utility/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Shipping.Web.Utility
+{
+    public static class Utility
+    {
+        public static Guid NewUUID() {
+            return Guid.NewGuid();
+        }
+
+        public static string GetStringParameter(object par1)
+        {
+            PropertyInfo[] fields = par1.GetType().GetProperties(); 
+            string result = "";
+
+            foreach (var field in fields)
+            {
+                string name = field.Name;
+                result += "@" + name + ", ";
+            }
+
+            result = result.Substring(0, result.Length - 2);
+            return result;
+        }
+    }
+}
