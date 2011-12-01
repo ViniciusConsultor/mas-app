8fdc9c130f897eb36511a1fb217c379888a1d394
aegean4112 <aegean4112@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2011-12-01 04:32:53 +0000

    save solution

--- 0.1.0/Shipping.Business.Entities/Business Object/Category.cs
+++ 0.1.0/Shipping.Business.Entities/Business Object/Category.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Linq.Mapping;
 
 namespace Shipping.Business.Entities
 {
+    [Table(Name="Category")]
     public class Category
     {
-        public virtual Guid Id { set; get; }
+
+        [Column(Name="category_code", IsPrimaryKey=true)]
         public string CategoryCode { set; get; }
+
+        [Column(Name="category_name")]
         public string CategoryName { set; get; }
     }
 }
--- 0.1.0/Shipping.Business.Entities/Business Object/Condition.cs
+++ 0.1.0/Shipping.Business.Entities/Business Object/Condition.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Linq.Mapping;
 
 namespace Shipping.Business.Entities
 {
+    [Table(Name="Condition")]
     public class Condition
-    {
-        public virtual Guid Id { get; set; }
+    { 
+        [Column(Name="condition_code", IsPrimaryKey=true)]
         public string ConditionCode { get; set; }
+
+        [Column(Name="condition_name")]
         public string ConditionName { get; set; }
     }
 }
