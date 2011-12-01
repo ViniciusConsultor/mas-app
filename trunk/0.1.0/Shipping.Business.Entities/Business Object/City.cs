c446ccf8dba6628d3f5f97e8f733da2123778dc7
aegean4112 <aegean4112@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2011-12-01 04:32:53 +0000

    save solution

--- trunk/0.1.0/Shipping.Business.Entities/Business Object/City.cs
+++ trunk/0.1.0/Shipping.Business.Entities/Business Object/City.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Linq.Mapping;
 
 namespace Shipping.Business.Entities
 {
+    [Table(Name="City")]
     public class City
     {
-        public virtual Guid Id { get; set; }
+
+        [Column(Name="city_code", IsPrimaryKey=true)]
         public string CityCode { get; set; }
+
+        [Column(Name="city_name")]
         public string CityName { get; set; }
     }
 }
