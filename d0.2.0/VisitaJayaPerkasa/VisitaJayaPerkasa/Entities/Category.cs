b097f1c3bcbf261660454708fab40e92da1bb55d
ahmad.jmlddn <ahmad.jmlddn@gmail.com>
2011-12-20 07:34:13 +0000

    fixed conflict

---
+++ d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisitaJayaPerkasa.Entities
+{
+    public class Category
+    {
+        public Guid ID { get; set; }
+        public string CategoryCode { set; get; }
+        public string CategoryName { set; get; }
+        public int Deleted { get; set; }
+    }
+    
+}
