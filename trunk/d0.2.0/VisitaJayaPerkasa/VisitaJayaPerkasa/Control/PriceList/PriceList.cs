ded6cb7e8a472ff54eb04ca387335a9fc6a05d22
aegean4112 <aegean4112@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2011-12-21 04:44:33 +0000

    add searchlist and price List

--- trunk/d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Control/PriceList/PriceList.cs
+++ trunk/d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Control/PriceList/PriceList.cs
@@ -8,33 +8,81 @@ using System.Text;
 using System.Windows.Forms;
 using VisitaJayaPerkasa.SqlRepository;
 using VisitaJayaPerkasa.Entities;
+using Telerik.WinControls.UI;
 
 namespace VisitaJayaPerkasa.Control.PriceList
 {
     public partial class PriceList : UserControl
     {
         private SqlPriceListRepository sqlPriceListRepository;
- 
+        private SqlSupplierRepository sqlSupplierRepository;
+        private SqlCityRepository sqlCityRepository;
+        private SqlTypeContRepository sqlTypeContRepository;
+        private SqlConditionRepository sqlConditionRepository;
+
+        private List<VisitaJayaPerkasa.Entities.Category> listTypeOfSupplier;
+        private List<VisitaJayaPerkasa.Entities.Supplier> listSupplier; 
+        private List<VisitaJayaPerkasa.Entities.City> listCity;
+        private List<VisitaJayaPerkasa.Entities.TypeCont> listType;
+        private List<VisitaJayaPerkasa.Entities.Condition> listCondition;
+
         public PriceList()
         {
             InitializeComponent();
             sqlPriceListRepository = new SqlPriceListRepository();
+            sqlCityRepository = new SqlCityRepository();
 
-            List<VisitaJayaPerkasa.Entities.Category> listTypeOfSupplier = sqlPriceListRepository.GetTypeOfSupplier();
-
+            listTypeOfSupplier = sqlPriceListRepository.GetTypeOfSupplier();
             cboTypeSupplier.DataSource = listTypeOfSupplier;
             cboTypeSupplier.DisplayMember = "CategoryName";
             cboTypeSupplier.ValueMember = "ID";
             cboTypeSupplier.SelectedIndex = -1;
-            cboTypeSupplier.SelectedText = "-- Choose --";
+            cboTypeSupplier.SelectedText = Constant.VisitaJayaPerkasaApplication.cboDefaultText;
+
+            listCity = sqlCityRepository.GetCity();
+            cbDestination.DataSource = listCity;
+            cbDestination.DisplayMember = "CityName";
+            cbDestination.ValueMember = "ID";
+            cbDestination.SelectedIndex = -1;
+            cbDestination.SelectedText = Constant.VisitaJayaPerkasaApplication.cboDefaultText;
 
             cbSupplier.Enabled = false;
             cboRecipient.Enabled = false;
-            cboStuffingPlace.Enabled = false;
-            cbDestination.Enabled = false;
+            sqlPriceListRepository = null;
+            PriceListGridView.Enabled = false;
 
             sqlPriceListRepository = null;
-            listTypeOfSupplier = null;
+            sqlCityRepository = null;
+        }
+
+        private void LoadCboGridView() {
+            sqlSupplierRepository = new SqlSupplierRepository();
+            sqlCityRepository = new SqlCityRepository();
+            sqlTypeContRepository = new SqlTypeContRepository();
+            sqlConditionRepository = new SqlConditionRepository();
+
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[1]).DataSource = listSupplier;
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[1]).DisplayMember = "SupplierName";
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[1]).ValueMember = "ID";
+
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[2]).DataSource = listCity;
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[2]).DisplayMember = "CityName";
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[2]).ValueMember = "ID";
+
+            listType = sqlTypeContRepository.GetTypeCont();
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[3]).DataSource = listType;
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[3]).DisplayMember = "TypeName";
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[3]).ValueMember = "ID";
+
+            listCondition = sqlConditionRepository.GetCondition();
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[4]).DataSource = listCondition;
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[4]).DisplayMember = "ConditionName";
+            ((GridViewComboBoxColumn)this.PriceListGridView.Columns[4]).ValueMember = "ID";
+
+            sqlSupplierRepository = null;
+            sqlCityRepository = null;
+            sqlTypeContRepository = null;
+            sqlConditionRepository = null;
         }
 
         private void cboTypeSupplier_KeyPress(object sender, KeyPressEventArgs e)
@@ -66,8 +114,20 @@ namespace VisitaJayaPerkasa.Control.PriceList
         { 
             if ((!cboTypeSupplier.SelectedText.Equals("-- Choose --")) && (!cboTypeSupplier.SelectedText.Equals("")))
             {
+                if (PriceListGridView.RowCount > 0) {
+                    DialogResult dResult = MessageBox.Show(this, "Changing your data grid will not be save ? ", "Confirmation", MessageBoxButtons.YesNo);
+
+                    if (dResult == DialogResult.Yes)
+                    {
+                        PriceListGridView.Rows.Clear();
+                        PriceListGridView.Enabled = false;
+                    }
+                    else
+                        return;
+                }
+
                 sqlPriceListRepository = new SqlPriceListRepository();
-                List<VisitaJayaPerkasa.Entities.Supplier> listSupplier = sqlPriceListRepository.GetSupplier(Utility.Utility.ConvertToUUID(cboTypeSupplier.SelectedValue.ToString()));
+                listSupplier = sqlPriceListRepository.GetSupplier(Utility.Utility.ConvertToUUID(cboTypeSupplier.SelectedValue.ToString()));
 
                 cbSupplier.Enabled = true;
                 cbSupplier.DataSource = listSupplier;
@@ -76,8 +136,37 @@ namespace VisitaJayaPerkasa.Control.PriceList
                 cbSupplier.SelectedIndex = -1;
                 cbSupplier.Text = "-- Choose --";
 
-                listSupplier = null;
                 sqlPriceListRepository = null;
+            }
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            if (PriceListGridView.RowCount > 0)
+            {
+                DialogResult dResult = MessageBox.Show(this, "Changing your data grid will not be save ? ", "Confirmation", MessageBoxButtons.YesNo);
+
+                if (dResult == DialogResult.Yes)
+                {
+                    PriceListGridView.Rows.Clear();
+                }
+                else
+                    return;
+            }
+
+            PriceListGridView.Enabled = true;
+            if (cboTypeSupplier.Text.Equals(VisitaJayaPerkasa.Constant.VisitaJayaPerkasaApplication.cboDefaultText))
+                MessageBox.Show(this, "Please choose type of supplier", "Information");
+            else if (cbSupplier.Text.Equals(VisitaJayaPerkasa.Constant.VisitaJayaPerkasaApplication.cboDefaultText))
+                MessageBox.Show(this, "Please choose supplier", "Information");
+            else if (cbDestination.Text.Equals(VisitaJayaPerkasa.Constant.VisitaJayaPerkasaApplication.cboDefaultText))
+                MessageBox.Show(this, "Please choose destination", "Information");
+            else if (pickerFrom.Value.Date < pickerTo.Value.Date)
+                MessageBox.Show(this, "Date from must not be greather than date to", "Information");
+            else
+            {
+                PriceListGridView.Enabled = true;
+                LoadCboGridView();
             }
         }
     }
---
+++ trunk/d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Entities/Condition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisitaJayaPerkasa.Entities
+{
+    public class Condition
+    {
+        public Guid ID { get; set; }
+        public string ConditionCode { get; set; }
+        public string conditionName { get; set; }
+        public int Deleted { get; set; }
+    }
+}
