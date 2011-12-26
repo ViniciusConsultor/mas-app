50f6f41c7685d3dbb8ab6be163631328ad0fcc7d
firstiar@gmail.com <firstiar@gmail.com@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2011-12-26 11:51:11 +0000

    retouch the progressbar dialog to make it more informative.

--- trunk/d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Form/PBarDialog.Designer.cs
+++ trunk/d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Form/PBarDialog.Designer.cs
@@ -28,42 +28,74 @@ namespace VisitaJayaPerkasa.Form
         /// </summary>
         private void InitializeComponent()
         {
+            this.radPanel1 = new Telerik.WinControls.UI.RadPanel();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).BeginInit();
+            this.radPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
+            // 
+            // radPanel1
+            // 
+            this.radPanel1.Controls.Add(this.progressBar1);
+            this.radPanel1.Location = new System.Drawing.Point(12, 35);
+            this.radPanel1.Name = "radPanel1";
+            this.radPanel1.Size = new System.Drawing.Size(469, 25);
+            this.radPanel1.TabIndex = 0;
+            this.radPanel1.Text = "radPanel1";
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Location = new System.Drawing.Point(13, 13);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(81, 16);
+            this.radLabel1.TabIndex = 1;
+            this.radLabel1.Text = "Loading data...";
             // 
             // progressBar1
             // 
             this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.progressBar1.Location = new System.Drawing.Point(0, 0);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(455, 53);
+            this.progressBar1.Size = new System.Drawing.Size(469, 25);
             this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
-            this.progressBar1.TabIndex = 0;
+            this.progressBar1.TabIndex = 4;
             // 
             // PBarDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 53);
-            this.Controls.Add(this.progressBar1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(220)))));
+            this.ClientSize = new System.Drawing.Size(500, 110);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.radPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PBarDialog";
             // 
             // 
             // 
             this.RootElement.ApplyShapeToControl = true;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "PBarDialog";
             this.ThemeName = "ControlDefault";
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).EndInit();
+            this.radPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private Telerik.WinControls.UI.RadPanel radPanel1;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
         private System.Windows.Forms.ProgressBar progressBar1;
+
+
     }
 }
 
