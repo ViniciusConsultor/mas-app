2ba8b4e7fd40ec2249093fced6c39d48077292c4
ahmad.jmlddn <ahmad.jmlddn@gmail.com>
2011-12-27 03:52:47 +0000

    fixed pelayaran

--- d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Control/Pelayaran/PelayaranView.Designer.cs
+++ d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Control/Pelayaran/PelayaranView.Designer.cs
@@ -28,23 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn1 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn2 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridSortField gridSortField1 = new Telerik.WinControls.UI.GridSortField();
             this.lblPelayaranName = new Telerik.WinControls.UI.RadLabel();
             this.radToolStripElement1 = new Telerik.WinControls.UI.RadToolStripElement();
             this.radToolStripItem1 = new Telerik.WinControls.UI.RadToolStripItem();
             this.radButtonElement1 = new Telerik.WinControls.UI.RadButtonElement();
-            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
             this.radToolStrip1 = new Telerik.WinControls.UI.RadToolStrip();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.PelayaranDetailGridView = new Telerik.WinControls.UI.RadGridView();
             ((System.ComponentModel.ISupportInitialize)(this.lblPelayaranName)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radToolStrip1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PelayaranDetailGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // lblPelayaranName
             // 
-            this.lblPelayaranName.Location = new System.Drawing.Point(170, 73);
+            this.lblPelayaranName.Location = new System.Drawing.Point(160, 28);
             this.lblPelayaranName.Name = "lblPelayaranName";
-            this.lblPelayaranName.Size = new System.Drawing.Size(2, 2);
-            this.lblPelayaranName.TabIndex = 22;
+            this.lblPelayaranName.Size = new System.Drawing.Size(63, 16);
+            this.lblPelayaranName.TabIndex = 26;
+            this.lblPelayaranName.Text = "User Name";
             // 
             // radToolStripElement1
             // 
@@ -70,13 +78,13 @@
             this.radButtonElement1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.radButtonElement1.Click += new System.EventHandler(this.radButtonElement1_Click);
             // 
-            // radLabel2
+            // radLabel1
             // 
-            this.radLabel2.Location = new System.Drawing.Point(31, 73);
-            this.radLabel2.Name = "radLabel2";
-            this.radLabel2.Size = new System.Drawing.Size(90, 16);
-            this.radLabel2.TabIndex = 23;
-            this.radLabel2.Text = "Pelayaran Name";
+            this.radLabel1.Location = new System.Drawing.Point(20, 28);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(90, 16);
+            this.radLabel1.TabIndex = 17;
+            this.radLabel1.Text = "Pelayaran Name";
             // 
             // radToolStrip1
             // 
@@ -95,22 +103,70 @@
             this.radToolStrip1.RootElement.AutoSizeMode = Telerik.WinControls.RadAutoSizeMode.WrapAroundChildren;
             this.radToolStrip1.RootElement.MinSize = new System.Drawing.Size(5, 5);
             this.radToolStrip1.ShowOverFlowButton = true;
-            this.radToolStrip1.Size = new System.Drawing.Size(521, 49);
-            this.radToolStrip1.TabIndex = 20;
+            this.radToolStrip1.Size = new System.Drawing.Size(695, 27);
+            this.radToolStrip1.TabIndex = 16;
             this.radToolStrip1.Text = "radToolStrip1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radLabel1);
+            this.groupBox1.Controls.Add(this.lblPelayaranName);
+            this.groupBox1.Location = new System.Drawing.Point(14, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(444, 213);
+            this.groupBox1.TabIndex = 35;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Master Data";
+            // 
+            // PelayaranDetailGridView
+            // 
+            this.PelayaranDetailGridView.BackColor = System.Drawing.SystemColors.Control;
+            this.PelayaranDetailGridView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.PelayaranDetailGridView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PelayaranDetailGridView.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.PelayaranDetailGridView.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.PelayaranDetailGridView.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.PelayaranDetailGridView.Location = new System.Drawing.Point(0, 355);
+            // 
+            // 
+            // 
+            this.PelayaranDetailGridView.MasterGridViewTemplate.AllowAddNewRow = false;
+            gridViewTextBoxColumn1.FieldName = "VesselCode";
+            gridViewTextBoxColumn1.HeaderText = "Vessel Code";
+            gridViewTextBoxColumn1.UniqueName = "VesselCode";
+            gridViewTextBoxColumn1.Width = 130;
+            gridViewTextBoxColumn2.FieldName = "VesselName";
+            gridViewTextBoxColumn2.HeaderText = "Vessel Name";
+            gridViewTextBoxColumn2.SortOrder = Telerik.WinControls.UI.RadSortOrder.Ascending;
+            gridViewTextBoxColumn2.UniqueName = "VesselName";
+            gridViewTextBoxColumn2.Width = 130;
+            this.PelayaranDetailGridView.MasterGridViewTemplate.Columns.Add(gridViewTextBoxColumn1);
+            this.PelayaranDetailGridView.MasterGridViewTemplate.Columns.Add(gridViewTextBoxColumn2);
+            gridSortField1.FieldAlias = "VesselName";
+            gridSortField1.FieldName = "VesselName";
+            gridSortField1.SortOrder = Telerik.WinControls.UI.RadSortOrder.Ascending;
+            this.PelayaranDetailGridView.MasterGridViewTemplate.SortExpressions.Add(gridSortField1);
+            this.PelayaranDetailGridView.Name = "PelayaranDetailGridView";
+            this.PelayaranDetailGridView.ReadOnly = true;
+            this.PelayaranDetailGridView.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.PelayaranDetailGridView.Size = new System.Drawing.Size(695, 284);
+            this.PelayaranDetailGridView.TabIndex = 36;
             // 
             // PelayaranView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.lblPelayaranName);
-            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.PelayaranDetailGridView);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.radToolStrip1);
             this.Name = "PelayaranView";
-            this.Size = new System.Drawing.Size(521, 397);
+            this.Size = new System.Drawing.Size(695, 639);
             ((System.ComponentModel.ISupportInitialize)(this.lblPelayaranName)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radToolStrip1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PelayaranDetailGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,7 +178,9 @@
         private Telerik.WinControls.UI.RadToolStripElement radToolStripElement1;
         private Telerik.WinControls.UI.RadToolStripItem radToolStripItem1;
         private Telerik.WinControls.UI.RadButtonElement radButtonElement1;
-        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
         private Telerik.WinControls.UI.RadToolStrip radToolStrip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Telerik.WinControls.UI.RadGridView PelayaranDetailGridView;
     }
 }
--- d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Control/Pelayaran/PelayaranView.cs
+++ d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Control/Pelayaran/PelayaranView.cs
@@ -5,15 +5,26 @@ using System.Drawing;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Windows.Forms; 
+using System.Windows.Forms;
+using VisitaJayaPerkasa.SqlRepository; 
 
 namespace VisitaJayaPerkasa.Control.Pelayaran
 {
     public partial class PelayaranView : UserControl
     {
+        private SqlPelayaranRepository sqlPelayaranRepository;
+        private List<VisitaJayaPerkasa.Entities.PelayaranDetail> listPelayaranDetail;
+
         public PelayaranView(VisitaJayaPerkasa.Entities.Pelayaran pelayaran)
         {
             InitializeComponent();
+            sqlPelayaranRepository = new SqlPelayaranRepository();
+            listPelayaranDetail = sqlPelayaranRepository.ListPelayaranDetail(pelayaran.ID);
+
+            if (listPelayaranDetail != null)
+                PelayaranDetailGridView.DataSource = listPelayaranDetail;
+
+
             lblPelayaranName.Text = pelayaran.Name;
         }
 
