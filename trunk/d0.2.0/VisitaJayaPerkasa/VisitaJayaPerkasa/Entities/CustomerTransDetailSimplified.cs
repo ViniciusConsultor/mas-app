2821c56e28fc70873bf1ec97cb69545c3022db39
firstiar@gmail.com <firstiar@gmail.com@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2012-04-13 06:28:50 +0000

    adding CustomerTransDetailSimplified

---
+++ trunk/d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Entities/CustomerTransDetailSimplified.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisitaJayaPerkasa.Entities
+{
+    public class CustomerTransDetailSimplified : CustomerTrans
+    {
+        public Guid CustomerDetailTransID { get; set; }
+        //model field for view
+        public string TypeName { get; set; }
+        public string VesselName { get; set; }
+        public string OriginName { get; set; }
+        public string DestinationName { get; set; }
+        public string ConditionName { get; set; }
+        public string WarehouseName { get; set; }
+        public string RecipientName { get; set; }
+        public string StringRep
+        {
+            get { return TypeName + ";" + OriginName + ">" + DestinationName + ";" + RecipientName; }
+        }
+    }
+}
