9e49174ea25b00e78a0e4d069acba55068e84754
aegean4112 <aegean4112@0e0badb5-6b7f-c821-d43d-de4d0a517da4>
2012-05-22 02:52:44 +0000

    just only add UI for report (not logic)

---
+++ d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Form/Report/ChangeDestination/RFChangeDestination.Designer.cs
@@ -0,0 +1,355 @@
+namespace VisitaJayaPerkasa.Form.Report.ChangeDestination
+{
+    partial class RFChangeDestination
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new Telerik.WinControls.UI.RadButton();
+            this.btnCancel = new Telerik.WinControls.UI.RadButton();
+            this.cboCustomer = new Telerik.WinControls.UI.RadComboBox();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.etNoSurat = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel5 = new Telerik.WinControls.UI.RadLabel();
+            this.radComboBox1 = new Telerik.WinControls.UI.RadComboBox();
+            this.radLabel3 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel4 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel6 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel7 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel8 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel9 = new Telerik.WinControls.UI.RadLabel();
+            this.radComboBox2 = new Telerik.WinControls.UI.RadComboBox();
+            this.radComboBox3 = new Telerik.WinControls.UI.RadComboBox();
+            this.radComboBox4 = new Telerik.WinControls.UI.RadComboBox();
+            this.radComboBox5 = new Telerik.WinControls.UI.RadComboBox();
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radTextBox2 = new Telerik.WinControls.UI.RadTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCancel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cboCustomer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.etNoSurat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(52, 231);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 22);
+            this.btnSave.TabIndex = 52;
+            this.btnSave.Text = "Save";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(166, 231);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 22);
+            this.btnCancel.TabIndex = 51;
+            this.btnCancel.Text = "Cancel";
+            // 
+            // cboCustomer
+            // 
+            this.cboCustomer.Location = new System.Drawing.Point(145, 66);
+            this.cboCustomer.Name = "cboCustomer";
+            // 
+            // 
+            // 
+            this.cboCustomer.RootElement.AutoSizeMode = Telerik.WinControls.RadAutoSizeMode.WrapAroundChildren;
+            this.cboCustomer.RootElement.StretchVertically = true;
+            this.cboCustomer.Size = new System.Drawing.Size(150, 20);
+            this.cboCustomer.TabIndex = 50;
+            this.cboCustomer.TabStop = false;
+            this.cboCustomer.Text = "-- Choose --";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Location = new System.Drawing.Point(4, 66);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(48, 16);
+            this.radLabel2.TabIndex = 48;
+            this.radLabel2.Text = "Supplier";
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Location = new System.Drawing.Point(4, 44);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(55, 16);
+            this.radLabel1.TabIndex = 47;
+            this.radLabel1.Text = "Container";
+            // 
+            // etNoSurat
+            // 
+            this.etNoSurat.Location = new System.Drawing.Point(145, 18);
+            this.etNoSurat.Name = "etNoSurat";
+            this.etNoSurat.ReadOnly = true;
+            this.etNoSurat.Size = new System.Drawing.Size(142, 20);
+            this.etNoSurat.TabIndex = 46;
+            this.etNoSurat.TabStop = false;
+            // 
+            // radLabel5
+            // 
+            this.radLabel5.Location = new System.Drawing.Point(4, 22);
+            this.radLabel5.Name = "radLabel5";
+            this.radLabel5.Size = new System.Drawing.Size(23, 16);
+            this.radLabel5.TabIndex = 45;
+            this.radLabel5.Text = "Hal";
+            // 
+            // radComboBox1
+            // 
+            this.radComboBox1.Location = new System.Drawing.Point(145, 44);
+            this.radComboBox1.Name = "radComboBox1";
+            // 
+            // 
+            // 
+            this.radComboBox1.RootElement.AutoSizeMode = Telerik.WinControls.RadAutoSizeMode.WrapAroundChildren;
+            this.radComboBox1.RootElement.StretchVertically = true;
+            this.radComboBox1.Size = new System.Drawing.Size(150, 20);
+            this.radComboBox1.TabIndex = 53;
+            this.radComboBox1.TabStop = false;
+            this.radComboBox1.Text = "-- Choose --";
+            // 
+            // radLabel3
+            // 
+            this.radLabel3.Location = new System.Drawing.Point(4, 88);
+            this.radLabel3.Name = "radLabel3";
+            this.radLabel3.Size = new System.Drawing.Size(63, 16);
+            this.radLabel3.TabIndex = 54;
+            this.radLabel3.Text = "Destination";
+            // 
+            // radLabel4
+            // 
+            this.radLabel4.Location = new System.Drawing.Point(4, 110);
+            this.radLabel4.Name = "radLabel4";
+            this.radLabel4.Size = new System.Drawing.Size(40, 16);
+            this.radLabel4.TabIndex = 55;
+            this.radLabel4.Text = "Vessel";
+            // 
+            // radLabel6
+            // 
+            this.radLabel6.Location = new System.Drawing.Point(4, 132);
+            this.radLabel6.Name = "radLabel6";
+            this.radLabel6.Size = new System.Drawing.Size(49, 16);
+            this.radLabel6.TabIndex = 56;
+            this.radLabel6.Text = "RO Milik";
+            // 
+            // radLabel7
+            // 
+            this.radLabel7.Location = new System.Drawing.Point(4, 154);
+            this.radLabel7.Name = "radLabel7";
+            this.radLabel7.Size = new System.Drawing.Size(62, 16);
+            this.radLabel7.TabIndex = 57;
+            this.radLabel7.Text = "RO dipakai";
+            // 
+            // radLabel8
+            // 
+            this.radLabel8.Location = new System.Drawing.Point(4, 176);
+            this.radLabel8.Name = "radLabel8";
+            this.radLabel8.Size = new System.Drawing.Size(65, 16);
+            this.radLabel8.TabIndex = 58;
+            this.radLabel8.Text = "Dari Tujuan";
+            // 
+            // radLabel9
+            // 
+            this.radLabel9.Location = new System.Drawing.Point(4, 198);
+            this.radLabel9.Name = "radLabel9";
+            this.radLabel9.Size = new System.Drawing.Size(93, 16);
+            this.radLabel9.TabIndex = 59;
+            this.radLabel9.Text = "Dipakai ke tujuan";
+            // 
+            // radComboBox2
+            // 
+            this.radComboBox2.Location = new System.Drawing.Point(145, 88);
+            this.radComboBox2.Name = "radComboBox2";
+            // 
+            // 
+            // 
+            this.radComboBox2.RootElement.AutoSizeMode = Telerik.WinControls.RadAutoSizeMode.WrapAroundChildren;
+            this.radComboBox2.RootElement.StretchVertically = true;
+            this.radComboBox2.Size = new System.Drawing.Size(150, 20);
+            this.radComboBox2.TabIndex = 60;
+            this.radComboBox2.TabStop = false;
+            this.radComboBox2.Text = "-- Choose --";
+            // 
+            // radComboBox3
+            // 
+            this.radComboBox3.Location = new System.Drawing.Point(145, 110);
+            this.radComboBox3.Name = "radComboBox3";
+            // 
+            // 
+            // 
+            this.radComboBox3.RootElement.AutoSizeMode = Telerik.WinControls.RadAutoSizeMode.WrapAroundChildren;
+            this.radComboBox3.RootElement.StretchVertically = true;
+            this.radComboBox3.Size = new System.Drawing.Size(150, 20);
+            this.radComboBox3.TabIndex = 61;
+            this.radComboBox3.TabStop = false;
+            this.radComboBox3.Text = "-- Choose --";
+            // 
+            // radComboBox4
+            // 
+            this.radComboBox4.Location = new System.Drawing.Point(145, 176);
+            this.radComboBox4.Name = "radComboBox4";
+            // 
+            // 
+            // 
+            this.radComboBox4.RootElement.AutoSizeMode = Telerik.WinControls.RadAutoSizeMode.WrapAroundChildren;
+            this.radComboBox4.RootElement.StretchVertically = true;
+            this.radComboBox4.Size = new System.Drawing.Size(150, 20);
+            this.radComboBox4.TabIndex = 62;
+            this.radComboBox4.TabStop = false;
+            this.radComboBox4.Text = "-- Choose --";
+            // 
+            // radComboBox5
+            // 
+            this.radComboBox5.Location = new System.Drawing.Point(145, 198);
+            this.radComboBox5.Name = "radComboBox5";
+            // 
+            // 
+            // 
+            this.radComboBox5.RootElement.AutoSizeMode = Telerik.WinControls.RadAutoSizeMode.WrapAroundChildren;
+            this.radComboBox5.RootElement.StretchVertically = true;
+            this.radComboBox5.Size = new System.Drawing.Size(150, 20);
+            this.radComboBox5.TabIndex = 51;
+            this.radComboBox5.TabStop = false;
+            this.radComboBox5.Text = "-- Choose --";
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.Location = new System.Drawing.Point(145, 132);
+            this.radTextBox1.Name = "radTextBox1";
+            this.radTextBox1.ReadOnly = true;
+            this.radTextBox1.Size = new System.Drawing.Size(142, 20);
+            this.radTextBox1.TabIndex = 63;
+            this.radTextBox1.TabStop = false;
+            // 
+            // radTextBox2
+            // 
+            this.radTextBox2.Location = new System.Drawing.Point(144, 154);
+            this.radTextBox2.Name = "radTextBox2";
+            this.radTextBox2.ReadOnly = true;
+            this.radTextBox2.Size = new System.Drawing.Size(142, 20);
+            this.radTextBox2.TabIndex = 47;
+            this.radTextBox2.TabStop = false;
+            // 
+            // RFChangeDestination
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 266);
+            this.Controls.Add(this.radTextBox2);
+            this.Controls.Add(this.radTextBox1);
+            this.Controls.Add(this.radComboBox5);
+            this.Controls.Add(this.radComboBox4);
+            this.Controls.Add(this.radComboBox3);
+            this.Controls.Add(this.radComboBox2);
+            this.Controls.Add(this.radLabel9);
+            this.Controls.Add(this.radLabel8);
+            this.Controls.Add(this.radLabel7);
+            this.Controls.Add(this.radLabel6);
+            this.Controls.Add(this.radLabel4);
+            this.Controls.Add(this.radLabel3);
+            this.Controls.Add(this.radComboBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.cboCustomer);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.etNoSurat);
+            this.Controls.Add(this.radLabel5);
+            this.Name = "RFChangeDestination";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "RFChangeDestination";
+            this.ThemeName = "ControlDefault";
+            ((System.ComponentModel.ISupportInitialize)(this.btnSave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCancel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cboCustomer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.etNoSurat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radComboBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadButton btnSave;
+        private Telerik.WinControls.UI.RadButton btnCancel;
+        private Telerik.WinControls.UI.RadComboBox cboCustomer;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadTextBox etNoSurat;
+        private Telerik.WinControls.UI.RadLabel radLabel5;
+        private Telerik.WinControls.UI.RadComboBox radComboBox1;
+        private Telerik.WinControls.UI.RadLabel radLabel3;
+        private Telerik.WinControls.UI.RadLabel radLabel4;
+        private Telerik.WinControls.UI.RadLabel radLabel6;
+        private Telerik.WinControls.UI.RadLabel radLabel7;
+        private Telerik.WinControls.UI.RadLabel radLabel8;
+        private Telerik.WinControls.UI.RadLabel radLabel9;
+        private Telerik.WinControls.UI.RadComboBox radComboBox2;
+        private Telerik.WinControls.UI.RadComboBox radComboBox3;
+        private Telerik.WinControls.UI.RadComboBox radComboBox4;
+        private Telerik.WinControls.UI.RadComboBox radComboBox5;
+        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadTextBox radTextBox2;
+    }
+}
+
---
+++ d0.2.0/VisitaJayaPerkasa/VisitaJayaPerkasa/Form/Report/ChangeDestination/RFChangeDestination.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace VisitaJayaPerkasa.Form.Report.ChangeDestination
+{
+    public partial class RFChangeDestination : Telerik.WinControls.UI.RadForm
+    {
+        public RFChangeDestination()
+        {
+            InitializeComponent();
+        }
+    }
+}
